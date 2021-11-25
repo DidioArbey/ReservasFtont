bfadfc341cce3d6576e0b75b3c24067eee4c75c7
didio <didioarbey@gmail.com>
2021-11-24 19:00:05 -0500

    creacion de los pages

---
+++ src/pages/BookingPage.js
@@ -0,0 +1,7 @@
+export default function BookingPage() {
+    return (
+        <div>
+            <h1>BookingPage</h1>
+        </div>
+    )
+}
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import AccountPage from "../pages/AccountPage"
+import BookingPage from "../pages/BookingPage"
+import BookingsPage from "../pages/BookingsPage"
+import HomePage from "../pages/HomePage"
+import LoginPage from "../pages/LoginPage"
+import NotFoundPage from "../pages/NotFoundPage"
+import RegisterPage from "../pages/RegisterPage"
+import UserPage from "../pages/admin/UserPage"
+
+
+export default function AppRouter() {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={HomePage}></Route>
+                <Route exact path="/login" component={LoginPage}/>
+                <Route exact path="/register" component={RegisterPage}/>
+                <Route exact path="/account" component={AccountPage}/>
+                <Route exact path="/bookings" component={BookingsPage}/>
+                <Route exact path="/booking/:bookingId" component={BookingPage}/>
+                <Route exact path="/admin/:users" component={UserPage}/>
+
+                <Route path="*" component={NotFoundPage}/>
+
+            </Switch>
+            <h1>AppRouter</h1>
+        </Router>
+    )
+}
+
