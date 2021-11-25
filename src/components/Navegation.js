fa8910b1037a05cfe2561a9dfc05862ee2a4992d
didio <didioarbey@gmail.com>
2021-11-24 22:18:10 -0500

    navbar con navegacion

--- src/components/Navegation.js
+++ src/components/Navegation.js
@@ -1,18 +1,19 @@
 import { Navbar, Nav, NavDropdown} from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 export default function Navegation() {
     return (
         <Navbar collapseOnSelect expand="lg" variant="dark" bg="dark">
-            <Navbar.Brand>Administrar reservas</Navbar.Brand>
+            <Navbar.Brand>Hotel Aroma cafetera</Navbar.Brand>
             <Navbar.Toggle arial-conrols="responsive-navbar-nav"/>
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link>Reservas</Nav.Link>
-                    <Nav.Link>Usuarios</Nav.Link>
+                    <Nav.Link as={NavLink} to="/bookings" >Reservas</Nav.Link>
+                    <Nav.Link as={NavLink} to="/admin/:users" >Usuarios</Nav.Link>
                 </Nav>
                 <Nav>
-                    <Nav.Link>Login</Nav.Link>
-                    <Nav.Link>Registrar</Nav.Link>
-                    <Nav.Link>Cuenta</Nav.Link>
+                    <Nav.Link as={NavLink} to="/login" >Login</Nav.Link>
+                    <Nav.Link as={NavLink} to="/register" >Registrar</Nav.Link>
+                    <Nav.Link as={NavLink} to="/account" >Cuenta</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
