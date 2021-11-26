0019a40d34108785b4f36949de2b3c2d3e3b6360
didio <didioarbey@gmail.com>
2021-11-25 22:07:36 -0500

    Navegacion con rutas e intento del logo

--- src/components/Navegation.js
+++ src/components/Navegation.js
@@ -3,7 +3,11 @@ import { NavLink } from 'react-router-dom'
 export default function Navegation() {
     return (
         <Navbar collapseOnSelect expand="lg" variant="dark" bg="dark">
-            <Navbar.Brand>Hotel Aroma cafetera</Navbar.Brand>
+            <Navbar.Brand as={NavLink} to="/">Hotel Aroma cafetera
+                <img src="/src/Image/Logo.png" width="30" height="30" className="d-inline-block align-top"/>
+            </Navbar.Brand>
+
+
             <Navbar.Toggle arial-conrols="responsive-navbar-nav"/>
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
