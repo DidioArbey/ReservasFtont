45f0f09d75a954d78c4f024eaf98b8868ef6e954
didio <didioarbey@gmail.com>
2021-11-27 20:56:42 -0500

    validacion si esta o no logueado

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -2,10 +2,10 @@ import { Redirect,Route } from 'react-router-dom';
 import useAuth from '../auth/useAuth';
 
 export default function PrivateRoute({hasRole: role, ...rest}) {
-    const {user} = useAuth();
-    if(role && user?.role !== role) return <Redirect to="/"/>
+    const {hasRole,isLogged} = useAuth();
+    if(role && !hasRole(role)) return <Redirect to="/"/>
 
-    if(!user) return <Redirect to="/login"/>
+    if(!isLogged()) return <Redirect to="/login"/>
     return (
         <Route {...rest} />
     )
--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -2,8 +2,8 @@ import { Redirect,Route } from 'react-router-dom';
 import useAuth from '../auth/useAuth';
 
 export default function PublicRoute(props) {
-    const {user} = useAuth();
-    if(user) return <Redirect to="/login"/>
+    const {isLogged} = useAuth();
+    if(isLogged()) return <Redirect to="/bookings"/>
     return (
         <Route {...props} />
     )
